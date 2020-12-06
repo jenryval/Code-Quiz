92a302f5469c772ad929631bae625bb5517050da
jenry valdes <jenryval93@gmail.com>
2020-12-05 18:38:40 -0600

    added questions for JS

---
+++ script.js
@@ -0,0 +1,30 @@
+// questions/answers to use
+
+Var questions = [{
+        title: "Who is the best Agent to play in Valorant if you want to block vision?",
+        Choices: ["Omen()", "Jet()", "Viper()", "Sage()"],
+        answer: "Omen()"
+    },
+    { 
+        title: "When in a 2V1 what is the best thing to do?",
+        Choices: ["push by yourself()", "hide()", "peak together()", "start making as much noise to attract opponenet()"],
+        answer: "peak together()"
+    },
+    {
+        title: "Best Fragger in Valorant?",
+        Choices: ["Asuna()", "Sinatra()", "wardell()", "crypticJA()"],
+        answer: "Asuna()"
+    },
+    {
+        title: "Best fragging Agent?",
+        Choices: ["Jett()", "Reyna()", "Phoenix()", "Sova()"],
+        answer: "Reyna()"
+
+    },
+    {
+        title: "Worst MAP MADE EVER",
+        Choices: ["Icebox()", "Haven()", "Split()", "Ascent()"],
+        answer: "Icebox()"
+    }
+
+]
